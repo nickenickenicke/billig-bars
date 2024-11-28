e3af22c99595adb902aa7a05f178110438339ab7
nickenickenicke <nickenickebergman@gmail.com>
2024-11-28 16:19:40 +0100

    Added locations suggestions

---
+++ src/lib/loactions.ts
@@ -0,0 +1,10 @@
+export const locationsSuggestions = {
+  medborgarplatsen: {
+    currentlat: 59.31493080050378,
+    currentlong: 18.072152827709495
+  },
+  bysistorget: {
+    currentlat: 59.31803212061374,
+    currentlong: 18.054961588411196
+  }
+}
