a614866f3a9811d8901c93c9948b7063b35017f2
nickenickenicke <nickenickebergman@gmail.com>
2024-11-28 16:25:00 +0100

    Added button to manually call the database

--- src/app/components/BarsList.tsx
+++ src/app/components/BarsList.tsx
@@ -3,14 +3,18 @@
 import { useContext } from 'react'
 import { BarsContext } from '../contexts/BarsContext'
 import { BarsListItem } from './BarsListItem'
+import { ReloadBarsButton } from './ReloadBarsButton'
 
 export const BarsList = () => {
   const { bars } = useContext(BarsContext)
   return (
-    <ul>
-      {bars.map(bar => (
-        <BarsListItem key={bar.id} bar={bar} />
-      ))}
-    </ul>
+    <>
+      <ul>
+        {bars.map(bar => (
+          <BarsListItem key={bar.id} bar={bar} />
+        ))}
+      </ul>
+      <ReloadBarsButton />
+    </>
   )
 }
---
+++ src/app/components/ReloadBarsButton.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useContext } from 'react'
+import { BarsContext } from '../contexts/BarsContext'
+import { getBars } from '../actions/getBars'
+import { BarActionType } from '../reducers/BarReducer'
+
+export const ReloadBarsButton = () => {
+  const { dispatch } = useContext(BarsContext)
+
+  const handleClick = async () => {
+    const newBars = await getBars()
+    dispatch({
+      type: BarActionType.UPDATED,
+      payload: JSON.stringify(newBars)
+    })
+  }
+
+  return (
+    <button type="button" onClick={handleClick}>
+      Initiate bars
+    </button>
+  )
+}
