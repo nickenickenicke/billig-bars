f5a4124bcb60d613a112c963111ab616d21d4608
nickenickenicke <nickenickebergman@gmail.com>
2024-11-28 14:40:14 +0100

    Cleanup

--- src/app/bars/page.tsx
+++ src/app/bars/page.tsx
@@ -1,20 +1,10 @@
 import { Bar } from '@/models/Bar'
-import { CurrentLocation } from '@/models/Location'
 import DisplayBars from '../components/DisplayBars'
-import { revalidatePath } from 'next/cache'
-import { getBarsByLocation } from '../actions/getBars'
-
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
+import { getBars } from '../actions/getBars'
 
 export default async function Countries() {
   let bars: Bar[] = []
-  const medborgarplatsen: CurrentLocation = {
-    currentlat: 59.31493080050378,
-    currentlong: 18.072152827709495
-  }
-  bars = await getBarsByLocation(medborgarplatsen)
+  bars = await getBars()
 
-  revalidatePath('/bars')
   return <>{<DisplayBars bars={bars} />}</>
 }
