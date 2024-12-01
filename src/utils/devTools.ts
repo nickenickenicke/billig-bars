2b598f5e9bcc80ae7a2708a7e69adf46c3e645f3
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-01 17:00:03 +0100

    Typo

--- src/utils/devTools.ts
+++ src/utils/devTools.ts
@@ -2,6 +2,6 @@ export const checkIfServerOrClient = (identifier: string) => {
   if (typeof window == 'undefined') {
     console.log(`${identifier} is on server side`)
   } else {
-    console.log(`${identifier} is on server side`)
+    console.log(`${identifier} is on client side`)
   }
 }
