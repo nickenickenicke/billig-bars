a4e313b45d3bc8cf552b2a04fafe71d268336869
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-01 16:40:41 +0100

    Created tool to check if we are on client or sever

---
+++ src/utils/devTools.ts
@@ -0,0 +1,7 @@
+export const checkIfServerOrClient = (identifier: string) => {
+  if (typeof window == 'undefined') {
+    console.log(`${identifier} is on server side`)
+  } else {
+    console.log(`${identifier} is on server side`)
+  }
+}
