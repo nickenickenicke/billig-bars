5105e3f56ccb551281f0f1b5e9a03bbf457482ef
nickenickenicke <nickenickebergman@gmail.com>
2024-12-02 13:31:21 +0100

    Refactor bar admin form into separate BarAdminForm component

--- src/app/bar-admin/page.tsx
+++ src/app/bar-admin/page.tsx
@@ -1,52 +1,5 @@
-import { insertBar } from '@/actions/formActions'
-import { FormHappyHours } from '@/components/form/FormHappyHours'
-import { FormInput } from '@/components/form/FormInput'
-import { FormLatLong } from '@/components/form/FormLatLong'
-import { FormOpeningHours } from '@/components/form/FormOpeningHours'
+import { BarAdminForm } from '@/components/BarAdminForm'
 
 export default async function Bars() {
-  return (
-    <form action={insertBar}>
-      <FormInput label={'Venue Name'} name={'name'} text placeholder="Barnamn" required />
-
-      <FormInput label={'Address'} name={'address'} text defaultValue="Hornsgatan " required />
-
-      <FormInput
-        label={'Postal Code'}
-        name={'postal_code'}
-        text
-        numericInput
-        defaultValue="118"
-        required
-      />
-
-      <FormInput label={'City'} name={'city'} text defaultValue="Stockholm" required />
-
-      <FormInput
-        label={'Beer Volume (cl)'}
-        name={'beer_volume'}
-        text
-        numericInput
-        defaultValue="40"
-        required
-      />
-
-      <FormInput
-        label={'Beer Price (SEK)'}
-        name={'beer_price'}
-        text
-        numericInput
-        defaultValue="39"
-        required
-      />
-
-      <FormLatLong />
-
-      <FormOpeningHours />
-
-      <FormHappyHours />
-
-      <button type="submit">Submit</button>
-    </form>
-  )
+  return <BarAdminForm />
 }
