b20bd1a90694dc896798b91cc6857d9d9acf9f2c
nickenickenicke <nickenickebergman@gmail.com>
2024-12-02 16:20:43 +0100

    Started work on BarListOpeningHours

---
+++ src/components/BarListItemOpeningHours.tsx
@@ -0,0 +1,9 @@
+import { OpeningHours } from '@/models/Bar'
+
+interface BarListItemOpeningHoursProps {
+  openingHours: OpeningHours[]
+}
+//TODO: Continue from here :D
+export const BarListItemOpeningHours = ({ openingHours }: BarListItemOpeningHoursProps) => {
+  return <></>
+}
