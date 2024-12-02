3bb64f1280ca035e9ccc5f7357bd934b9fbcf005
nickenickenicke <nickenickebergman@gmail.com>
2024-12-02 16:05:43 +0100

    Ppc work

--- src/components/BarsListItem.tsx
+++ src/components/BarsListItem.tsx
@@ -1,4 +1,5 @@
 import { Bar } from '@/models/Bar'
+import { getPricePerCl } from '@/utils/priceTools'
 
 interface BarsListItemProps {
   bar: Bar
@@ -9,7 +10,10 @@ export const BarsListItem = ({ bar }: BarsListItemProps) => {
     <li>
       <h2>{bar.name}</h2>
       <p>{bar.address}</p>
-      <p>{bar.beer_price}</p>
+      <p>{bar.postal_code.toString().replace(/(.{3})/, '$1 ') + ' ' + bar.city}</p>
+      <p>{bar.beer_price} kr</p>
+      <p>{bar.beer_volume} cl</p>
+      <p>Pris per cl: {getPricePerCl(bar.beer_price, bar.beer_volume)}kr</p>
     </li>
   )
 }
