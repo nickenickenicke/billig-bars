0727d4a3061565c8029e2d23e678ffb20e951972
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-03 12:44:58 +0100

    Normalize postal code

--- src/components/BarsListItem.tsx
+++ src/components/BarsListItem.tsx
@@ -2,6 +2,7 @@ import { Bar } from '@/models/Bar'
 import { getPricePerCl } from '@/utils/priceTools'
 import { BarListItemOpeningHours } from './BarListItemOpeningHours'
 import { BarListItemHappyHours } from './BarListItemHappyHours'
+import { normalizePostalCode } from '@/utils/locationTools'
 
 interface BarsListItemProps {
   bar: Bar
@@ -9,13 +10,16 @@ interface BarsListItemProps {
 
 export const BarsListItem = ({ bar }: BarsListItemProps) => {
   return (
-    <li>
+    <li className="border">
       <h2>{bar.name}</h2>
-      <p>{bar.address}</p>
-      <p>{bar.postal_code.toString().replace(/(.{3})/, '$1 ') + ' ' + bar.city}</p>
+      <address>
+        {bar.address}
+        <br />
+        {normalizePostalCode(bar.postal_code)} {bar.city}
+      </address>
       <p>{bar.beer_price} kr</p>
       <p>{bar.beer_volume} cl</p>
-      <p>Pris per cl: {getPricePerCl(bar.beer_price, bar.beer_volume)}kr</p>
+      <p>kr/cl: {getPricePerCl(bar.beer_price, bar.beer_volume)}kr</p>
       <BarListItemOpeningHours openingHours={bar.opening_hours} />
       {bar.happy_hours && <BarListItemHappyHours happyHours={bar.happy_hours} />}
     </li>
