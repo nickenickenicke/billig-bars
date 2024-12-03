26dfe283f15cbbcf086c362e6cc0b21e9fe11512
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-03 13:13:13 +0100

    Renamed parseTimeFromDB to normalizeTimeFromDB

--- src/components/BarListItemHappyHours.tsx
+++ src/components/BarListItemHappyHours.tsx
@@ -1,6 +1,6 @@
 import { HappyHours } from '@/models/Bar'
 import { getPricePerCl } from '@/utils/priceTools'
-import { getWeekdayName, parseTimeFromDB } from '@/utils/timeTools'
+import { getWeekdayName, normalizeTimeFromDB } from '@/utils/timeTools'
 
 interface BarListItemHappyHoursProps {
   happyHours: HappyHours[]
@@ -12,7 +12,7 @@ export const BarListItemHappyHours = ({ happyHours }: BarListItemHappyHoursProps
         {happyHours.map((happyHour, i) => {
           return (
             <li key={happyHour.id!}>
-              {`${getWeekdayName(happyHour.day_of_week)}: ${parseTimeFromDB(happyHour.starts_at)} - ${parseTimeFromDB(happyHour.starts_at)}`}
+              {`${getWeekdayName(happyHour.day_of_week)}: ${normalizeTimeFromDB(happyHour.starts_at)} - ${normalizeTimeFromDB(happyHour.starts_at)}`}
               <br />
               {`Volym: ${happyHour.volume}cl, pris: ${happyHour.price}kr, pris per cl: ${getPricePerCl(happyHour.price, happyHour.volume || 40)}kr`}
             </li>
