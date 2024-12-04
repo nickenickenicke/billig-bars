3c2cb8ac23f5bf072e0747b52a8025331c0b016b
nickenickenicke <nickenickebergman@gmail.com>
2024-12-04 14:49:20 +0100

    Refactor BarPage

--- src/app/bars/[id]/page.tsx
+++ src/app/bars/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { BarHappyHours } from '@/components/BarHappyHours'
 import { BarOpeningHours } from '@/components/BarOpeningHours'
 import { Bar } from '@/models/Bar'
-import { CurrentLocation } from '@/models/Location'
 import { normalizeMeters, normalizePostalCode } from '@/utils/locationTools'
 import { createClient } from '@/utils/supabase/server'
 import { checkIsHappyHour } from '@/utils/timeTools'
@@ -31,40 +30,37 @@ export default async function BarPage({ params, searchParams }: BarPageProps) {
     supabaseQuery.currentlong = parseFloat(searchParams.currentlong as string)
   }
 
-  //   const supabase = await createClient()
+  const supabase = await createClient()
 
-  //   let { data, error } = await supabase.rpc('get_bar_by_id', {
-  //     p_uuid: params.id
-  //   })
+  let { data, error } = await supabase.rpc('get_bar_by_id', supabaseQuery)
 
-  //   if (error) {
-  //     console.error(error)
-  //     redirect('/')
-  //   }
+  if (error) {
+    console.error(error)
+    redirect('/')
+  }
 
-  //   if (data) {
-  //     const bar: Bar = { ...data }
+  if (data) {
+    const bar: Bar = { ...data }
 
-  //     let isHappyHour = false
-  //     if (bar.happy_hours) isHappyHour = checkIsHappyHour(bar.happy_hours)
+    let isHappyHour = false
+    if (bar.happy_hours) isHappyHour = checkIsHappyHour(bar.happy_hours)
 
-  //     return (
-  //       <>
-  //         <h1>{bar.name}</h1>
-  //         {bar.dist_meters && bar.dist_meters > 0 ? <p>{normalizeMeters(bar.dist_meters)}</p> : null}
-  //         {isHappyHour && <span>HAPPY HOUR</span>}
-  //         <address>
-  //           {bar.address}
-  //           <br />
-  //           {normalizePostalCode(bar.postal_code)} {bar.city}
-  //         </address>
-  //         <BarOpeningHours openingHours={bar.opening_hours} />
-  //         {bar.happy_hours && <BarHappyHours happyHours={bar.happy_hours} />}
-  //         <pre className="text-xs">{JSON.stringify(bar, null, 2)}</pre>
-  //       </>
-  //     )
-  //   }
+    return (
+      <>
+        <h1>{bar.name}</h1>
+        {bar.dist_meters && bar.dist_meters > 0 ? <p>{normalizeMeters(bar.dist_meters)}</p> : null}
+        {isHappyHour && <span>HAPPY HOUR</span>}
+        <address>
+          {bar.address}
+          <br />
+          {normalizePostalCode(bar.postal_code)} {bar.city}
+        </address>
+        <BarOpeningHours openingHours={bar.opening_hours} />
+        {bar.happy_hours && <BarHappyHours happyHours={bar.happy_hours} />}
+        <pre className="text-xs">{JSON.stringify(bar, null, 2)}</pre>
+      </>
+    )
+  }
 
-  //   return redirect('/')
-  return <>{JSON.stringify(supabaseQuery)}</>
+  return redirect('/')
 }
