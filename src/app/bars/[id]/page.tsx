7427501bff907fd8db8f27b4773622a429c450aa
nickenickenicke <nickenickebergman@gmail.com>
2024-12-04 13:58:12 +0100

    Started work on currentlat & currentlong

--- src/app/bars/[id]/page.tsx
+++ src/app/bars/[id]/page.tsx
@@ -1,43 +1,70 @@
 import { BarHappyHours } from '@/components/BarHappyHours'
 import { BarOpeningHours } from '@/components/BarOpeningHours'
 import { Bar } from '@/models/Bar'
+import { CurrentLocation } from '@/models/Location'
 import { normalizeMeters, normalizePostalCode } from '@/utils/locationTools'
 import { createClient } from '@/utils/supabase/server'
+import { checkIsHappyHour } from '@/utils/timeTools'
 import { redirect } from 'next/navigation'
 
 interface BarPageProps {
   params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default async function BarPage({ params }: BarPageProps) {
-  const supabase = await createClient()
+type SupabaseQuery = {
+  p_uuid: string
+  currentlat?: number
+  currentlong?: number
+}
 
-  let { data, error } = await supabase.rpc('get_bar_by_id', {
+export default async function BarPage({ params, searchParams }: BarPageProps) {
+  const supabaseQuery: SupabaseQuery = {
     p_uuid: params.id
-  })
-
-  if (error) {
-    console.error(error)
-    redirect('/')
   }
 
-  if (data) {
-    const bar: Bar = { ...data }
-
-    return (
-      <>
-        <h1>{bar.name}</h1>
-        {bar.dist_meters && bar.dist_meters > 0 ? <p>{normalizeMeters(bar.dist_meters)}</p> : null}
-        <address>
-          {bar.address}
-          <br />
-          {normalizePostalCode(bar.postal_code)} {bar.city}
-        </address>
-        <BarOpeningHours openingHours={bar.opening_hours} />
-        {bar.happy_hours && <BarHappyHours happyHours={bar.happy_hours} />}
-        <pre className="text-xs">{JSON.stringify(bar, null, 2)}</pre>
-      </>
-    )
+  if (
+    parseFloat(searchParams.currentlat as string) > 0 &&
+    parseFloat(searchParams.currentlong as string) > 0
+  ) {
+    supabaseQuery.currentlat = parseFloat(searchParams.currentlat as string)
+    supabaseQuery.currentlong = parseFloat(searchParams.currentlong as string)
   }
-  return redirect('/')
+
+  //   const supabase = await createClient()
+
+  //   let { data, error } = await supabase.rpc('get_bar_by_id', {
+  //     p_uuid: params.id
+  //   })
+
+  //   if (error) {
+  //     console.error(error)
+  //     redirect('/')
+  //   }
+
+  //   if (data) {
+  //     const bar: Bar = { ...data }
+
+  //     let isHappyHour = false
+  //     if (bar.happy_hours) isHappyHour = checkIsHappyHour(bar.happy_hours)
+
+  //     return (
+  //       <>
+  //         <h1>{bar.name}</h1>
+  //         {bar.dist_meters && bar.dist_meters > 0 ? <p>{normalizeMeters(bar.dist_meters)}</p> : null}
+  //         {isHappyHour && <span>HAPPY HOUR</span>}
+  //         <address>
+  //           {bar.address}
+  //           <br />
+  //           {normalizePostalCode(bar.postal_code)} {bar.city}
+  //         </address>
+  //         <BarOpeningHours openingHours={bar.opening_hours} />
+  //         {bar.happy_hours && <BarHappyHours happyHours={bar.happy_hours} />}
+  //         <pre className="text-xs">{JSON.stringify(bar, null, 2)}</pre>
+  //       </>
+  //     )
+  //   }
+
+  //   return redirect('/')
+  return <>{JSON.stringify(supabaseQuery)}</>
 }
