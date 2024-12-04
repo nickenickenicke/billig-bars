a1b4ff5627f278b35e01ba42adc85d4777c28973
nickenickenicke <nickenickebergman@gmail.com>
2024-12-04 14:48:32 +0100

    Add singleBarMockData

--- src/lib/mockdata.ts
+++ src/lib/mockdata.ts
@@ -1,3 +1,4 @@
+import { Bar } from '@/models/Bar'
 import { GlobalState } from '@/models/GlobalState'
 import { defaultLocationState } from '@/reducers/GlobalStateReducer'
 
@@ -284,6 +285,121 @@ export const initialMockData: GlobalState = {
           day_of_week: 7
         }
       ]
+    }
+  ]
+}
+
+export const singleBarMockData: Bar = {
+  id: '1ec2221b-b252-4127-89d9-ecf7889ec554',
+  lat: 59.3076553344727,
+  city: 'Stockholm',
+  long: 18.0771446228027,
+  name: 'Amici Nostri',
+  address: 'Ringvägen 104',
+  beer_price: 59,
+  beer_volume: 40,
+  dist_meters: 1919.85051734,
+  happy_hours: [
+    {
+      id: 'd2cb3f58-5f36-4ef4-bdb0-afb1082ace10',
+      price: 39,
+      volume: 40,
+      ends_at: 17,
+      starts_at: 14,
+      day_of_week: 1
+    },
+    {
+      id: '8edbe7f5-b08b-4dbc-8784-e65b1351634f',
+      price: 39,
+      volume: 40,
+      ends_at: 17,
+      starts_at: 14,
+      day_of_week: 2
+    },
+    {
+      id: 'f88dab61-c169-450d-95f7-667872dabf19',
+      price: 39,
+      volume: 40,
+      ends_at: 17,
+      starts_at: 14,
+      day_of_week: 3
+    },
+    {
+      id: 'e2a7f2ba-e8e2-4de0-bec6-c14176ef7d8e',
+      price: 39,
+      volume: 40,
+      ends_at: 17,
+      starts_at: 14,
+      day_of_week: 4
+    },
+    {
+      id: '4467b85c-4e90-40dc-a05b-c3dcf039882c',
+      price: 39,
+      volume: 40,
+      ends_at: 17,
+      starts_at: 14,
+      day_of_week: 5
+    },
+    {
+      id: '27893708-5741-4a24-a896-82d2a0970b8f',
+      price: 39,
+      volume: 40,
+      ends_at: 17,
+      starts_at: 14,
+      day_of_week: 6
+    },
+    {
+      id: '79855ee3-f318-4f8e-a1e2-2f2763d30071',
+      price: 39,
+      volume: 40,
+      ends_at: 17,
+      starts_at: 14,
+      day_of_week: 7
+    }
+  ],
+  postal_code: 11661,
+  opening_hours: [
+    {
+      id: '43069954-f646-4162-be4c-d61120a1a921',
+      opens_at: 14,
+      closes_at: 1,
+      day_of_week: 1
+    },
+    {
+      id: '0250d4b7-44fc-49c6-9d61-eb4d100c0e4d',
+      opens_at: 14,
+      closes_at: 1,
+      day_of_week: 2
+    },
+    {
+      id: 'f9bc237f-2a06-4b6a-a44a-a2443b541dce',
+      opens_at: 14,
+      closes_at: 3,
+      day_of_week: 3
+    },
+    {
+      id: '4ee68286-0829-450a-98eb-aaf181b70993',
+      opens_at: 14,
+      closes_at: 1,
+      day_of_week: 4
+    },
+    {
+      id: 'd84e31ad-2f27-4056-989a-c1b013454d6c',
+      opens_at: 14,
+      closes_at: 3,
+      day_of_week: 5
+    },
+    {
+      id: '4cc008ca-3791-4c27-9452-d389161fe272',
+      opens_at: 14,
+      closes_at: 3,
+      day_of_week: 6
+    },
+    {
+      id: '975db969-5bf0-4dcf-859f-95f52fd8a21a',
+      opens_at: 14,
+      closes_at: 1,
+      day_of_week: 7
     }
   ]
 }
