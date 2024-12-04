45a71f60cb9910ab78c39560b10b743e293617c8
nickenickenicke <nickenickebergman@gmail.com>
2024-12-04 09:53:50 +0100

    Add BarCard component to display bar details and refactor BarsList

--- src/components/BarsList.tsx
+++ src/components/BarsList.tsx
@@ -5,11 +5,17 @@ import { BarsContext } from '@/contexts/BarsContext'
 import { BarsListItem } from './BarsListItem'
 import { ReloadBarsButton } from './ReloadBarsButton'
 import { GeolocateButton } from './GeolocateButton'
+import { BarCard } from './BarCard'
 
 export const BarsList = () => {
   const { bars } = useContext(BarsContext)
   return (
     <>
+      <article className="flex flex-col gap-2">
+        {bars.map(bar => (
+          <BarCard key={bar.id + 'card'} bar={bar} />
+        ))}
+      </article>
       <ul className="flex flex-col gap-4">
         {bars.map(bar => (
           <BarsListItem key={bar.id} bar={bar} />
