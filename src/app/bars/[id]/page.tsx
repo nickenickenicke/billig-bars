5e41d483c818fc487230a601a67586406bb830c4
nickenickenicke <nickenickebergman@gmail.com>
2024-12-04 12:37:00 +0100

    Fetch and display bar details by ID in BarPage component

--- src/app/bars/[id]/page.tsx
+++ src/app/bars/[id]/page.tsx
@@ -1,12 +1,34 @@
+import { Bar } from '@/models/Bar'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+
 interface BarPageProps {
   params: { id: string }
 }
 
 export default async function BarPage({ params }: BarPageProps) {
-  return (
-    <>
-      <h1>{JSON.stringify(params.id)}</h1>
-      <p>Hej!</p>
-    </>
-  )
+  const supabase = await createClient()
+
+  let { data, error } = await supabase.rpc('get_bar_by_id', {
+    p_uuid: params.id
+  })
+
+  if (error) {
+    console.error(error)
+    redirect('/')
+  }
+
+  if (data) {
+    const bar: Bar = { ...data }
+
+    return (
+      <>
+        <h1>{bar.name}</h1>
+        <p>Hej!</p>
+
+        <pre className="text-xs">{JSON.stringify(bar, null, 2)}</pre>
+      </>
+    )
+  }
+  return redirect('/')
 }
