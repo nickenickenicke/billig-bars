838e4d2dffb49418458c5cbd8320ec8ba89b496c
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-05 11:57:43 +0100

    Started work on fiflter by hour component

--- src/app/bars/page.tsx
+++ src/app/bars/page.tsx
@@ -1,10 +1,12 @@
 import { BarsList } from '@/components/BarsList'
+import { FilterBar } from '@/components/FilterBar'
 import { GeolocateButton } from '@/components/GeolocateButton'
 import { ReloadBarsButton } from '@/components/ReloadBarsButton'
 
 export default function Bars() {
   return (
     <>
+      <FilterBar />
       <ReloadBarsButton />
       <GeolocateButton />
       <BarsList />
---
+++ src/components/FilterBar.tsx
@@ -0,0 +1,10 @@
+import { FilterByHour } from '@/components/FilterByHour'
+
+export const FilterBar = () => {
+  return (
+    <>
+      <h3>FILTERING</h3>
+      <FilterByHour />
+    </>
+  )
+}
