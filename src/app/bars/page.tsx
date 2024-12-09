68391759a7e2ff756dc982ac9cde82eaa62bceb0
nickenickenicke <nickenickebergman@gmail.com>
2024-12-09 11:58:25 +0100

    Initial map work

--- src/app/bars/page.tsx
+++ src/app/bars/page.tsx
@@ -3,6 +3,7 @@ import { CheckSearchParams } from '@/components/CheckSearchParams'
 import { FilterBar } from '@/components/FilterBar'
 import { FilterStatus } from '@/components/FilterStatus'
 import { GeolocateButton } from '@/components/GeolocateButton'
+import { MapCanvas } from '@/components/MapCanvas'
 import { ReloadBarsButton } from '@/components/ReloadBarsButton'
 import { Suspense } from 'react'
 
@@ -12,6 +13,7 @@ export default function Bars() {
       <Suspense fallback={<p>Loading...</p>}>
         <CheckSearchParams />
       </Suspense>
+      <MapCanvas />
       <FilterBar />
       <ReloadBarsButton />
       <GeolocateButton />
---
+++ src/components/MapCanvas.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { GlobalStateContext } from '@/contexts/GlobalStateContext'
+import { useContext } from 'react'
+import { Map, Marker } from '@vis.gl/react-maplibre'
+import 'maplibre-gl/dist/maplibre-gl.css'
+
+export const MapCanvas = () => {
+  const {
+    globalState: { bars, currentLocation }
+  } = useContext(GlobalStateContext)
+
+  return (
+    <>
+      <Map
+        initialViewState={{
+          longitude: currentLocation.currentlong || 18.069215,
+          latitude: currentLocation.currentlat || 59.314654,
+          zoom: 13
+        }}
+        style={{ width: '100%', aspectRatio: '1/1' }}
+        mapStyle="https://tiles.openfreemap.org/styles/bright"
+      >
+        {bars.map(bar => (
+          <Marker key={bar.id} longitude={bar.long} latitude={bar.lat} anchor="bottom">
+            <span className="rounded-full bg-green-price p-2">{bar.beer_price}</span>
+          </Marker>
+        ))}
+      </Map>
+    </>
+  )
+}
