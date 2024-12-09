ef87c162b03fda46bf8072c8dfc9b896eedf4b1b
nickenickenicke <nickenickebergman@gmail.com>
2024-12-09 11:04:41 +0100

    Added suspense

--- src/app/bars/page.tsx
+++ src/app/bars/page.tsx
@@ -4,11 +4,14 @@ import { FilterBar } from '@/components/FilterBar'
 import { FilterStatus } from '@/components/FilterStatus'
 import { GeolocateButton } from '@/components/GeolocateButton'
 import { ReloadBarsButton } from '@/components/ReloadBarsButton'
+import { Suspense } from 'react'
 
 export default function Bars() {
   return (
     <>
-      <CheckSearchParams />
+      <Suspense fallback={<p>Loading...</p>}>
+        <CheckSearchParams />
+      </Suspense>
       <FilterBar />
       <ReloadBarsButton />
       <GeolocateButton />
