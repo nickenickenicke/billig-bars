d62a340d2e55d7356c380646430669238c74ca62
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-10 18:47:06 +0100

    Updated action to accept minutes

--- src/actions/formActions.ts
+++ src/actions/formActions.ts
@@ -6,27 +6,29 @@ import { formErrorToState } from '@/utils/errorTools'
 import { calculatePPV } from '@/utils/priceTools'
 import { createClient } from '@/utils/supabase/server'
 
-//TODO: Implement minutes
-
 export const insertBar = async (formState: FormState, formData: FormData): Promise<FormState> => {
   const openingHours: OpeningHours[] = []
   for (let index = 0; index < 7; index++) {
     let opens_at = formData.get(`opens_at_${index + 1}`)
+    let opens_at_min = formData.get(`opens_at_min_${index + 1}`)
     let closes_at = formData.get(`closes_at_${index + 1}`)
+    let closes_at_min = formData.get(`closes_at_min_${index + 1}`)
     if (opens_at && closes_at) {
       openingHours.push({
         day_of_week: index + 1,
         opens_at: parseInt(opens_at as string),
-        opens_at_min: 0,
+        opens_at_min: parseInt(opens_at_min as string),
         closes_at: parseInt(closes_at as string),
-        closes_at_min: 0
+        closes_at_min: parseInt(closes_at_min as string)
       })
     }
   }
   const happyHours: HappyHours[] = []
   for (let index = 0; index < 7; index++) {
     let starts_at = formData.get(`happy_starts_at_${index + 1}`)
+    let starts_at_min = formData.get(`happy_starts_at_min_${index + 1}`)
     let ends_at = formData.get(`happy_ends_at_${index + 1}`)
+    let ends_at_min = formData.get(`happy_ends_at_min_${index + 1}`)
     let volume = formData.get(`happy_volume_${index + 1}`)
     let price = formData.get(`happy_price_${index + 1}`)
     if (starts_at && ends_at && price && volume) {
@@ -34,9 +36,9 @@ export const insertBar = async (formState: FormState, formData: FormData): Promi
       happyHours.push({
         day_of_week: index + 1,
         starts_at: parseInt(starts_at as string),
-        starts_at_min: 0,
+        starts_at_min: parseInt(starts_at_min as string),
         ends_at: parseInt(ends_at as string),
-        ends_at_min: 0,
+        ends_at_min: parseInt(ends_at_min as string),
         price: parseInt(price as string),
         volume: parseInt(volume as string),
         ppv: ppv
