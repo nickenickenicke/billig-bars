35e5b6c6612b1877d813027dbb163a6928998235
nickenickenicke <nickenickebergman@gmail.com>
2024-12-11 15:41:35 +0100

    Refactor checkParams to use current time and day as defaults for URL parameters

--- src/utils/paramTools.ts
+++ src/utils/paramTools.ts
@@ -1,29 +1,30 @@
 import { CurrentQuery } from '@/models/GlobalState'
+import { getCurrentHour, getCurrentMinute, getTodaysWeekday } from './timeTools'
 
 export const checkParams = (params: URLSearchParams): CurrentQuery => {
   let desc = false
   if (params.get('sort') === 'desc') {
     desc = true
   }
-  let min: number | null = null
-  if (params.get('hour')) {
-    min = parseInt(params.get('min') as string) || null
-    if (min !== null && (min < 0 || min > 23)) {
-      min = null
+  let min: number | null = getCurrentMinute()
+  if (params.get('min')) {
+    min = parseInt(params.get('min') as string) || min
+    if (min !== null && (min < 0 || min > 59)) {
+      min = getCurrentMinute()
     }
   }
-  let hour: number | null = null
+  let hour: number | null = getCurrentHour()
   if (params.get('hour')) {
-    hour = parseInt(params.get('hour') as string) || null
+    hour = parseInt(params.get('hour') as string) || hour
     if (hour !== null && (hour < 0 || hour > 23)) {
-      hour = null
+      hour = getCurrentHour()
     }
   }
-  let day: number | null = null
+  let day: number | null = getTodaysWeekday()
   if (params.get('day')) {
-    day = parseInt(params.get('day') as string) || null
+    day = parseInt(params.get('day') as string) || day
     if (day !== null && (day < 0 || day > 7)) {
-      day = null
+      day = getTodaysWeekday()
     }
   }
 
