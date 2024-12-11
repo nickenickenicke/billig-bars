777af1e9d2b1fed7417dc329a808305468e16b6d
nickenickenicke <nickenickebergman@gmail.com>
2024-12-11 13:35:43 +0100

    Add popup functionality and refactor to use new components

--- src/components/MapCanvas.tsx
+++ src/components/MapCanvas.tsx
@@ -1,14 +1,32 @@
 'use client'
 
 import { GlobalStateContext } from '@/contexts/GlobalStateContext'
-import { useContext } from 'react'
-import { Map, Marker } from '@vis.gl/react-maplibre'
+import { useContext, useState } from 'react'
+import { Map } from '@vis.gl/react-maplibre'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import { MapPopup } from './MapPopup'
+import { MapBarMarker } from './MapBarMarker'
+import { MapCurrentLocationMarker } from './MapCurrentLocationMarker'
+import { MapLocation } from '@/models/Location'
 
 export const MapCanvas = () => {
   const {
     globalState: { bars, currentLocation }
   } = useContext(GlobalStateContext)
+  const [popupLocation, setPopupLocation] = useState<MapLocation>({
+    long: 0,
+    lat: 0
+  })
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+
+  const handleMarkerClick = (long: number, lat: number) => {
+    setPopupLocation({
+      long,
+      lat
+    })
+    setIsPopupOpen(true)
+  }
 
   return (
     <>
@@ -22,20 +40,23 @@ export const MapCanvas = () => {
         style={{ width: '100%', aspectRatio: '1/1' }}
         mapStyle="https://tiles.openfreemap.org/styles/bright"
       >
+        {isPopupOpen && (
+          <MapPopup
+            popupLocation={popupLocation}
+            handleClosePopup={() => {
+              setIsPopupOpen(false)
+            }}
+          />
+        )}
         {currentLocation.currentlat > 0 && currentLocation.currentlong > 0 ? (
-          <Marker
-            key={'currentlocation'}
-            longitude={currentLocation.currentlong}
-            latitude={currentLocation.currentlat}
-            anchor="bottom"
-          >
-            <span className="block aspect-square w-5 rounded-full bg-blue-500"></span>
-          </Marker>
+          <MapCurrentLocationMarker currentLocation={currentLocation} />
         ) : null}
         {bars.map(bar => (
-          <Marker key={bar.id} longitude={bar.long} latitude={bar.lat} anchor="bottom">
-            <span className="aspect-square rounded-full bg-green-price p-2">{bar.beer_price}</span>
-          </Marker>
+          <MapBarMarker
+            key={bar.id}
+            bar={bar}
+            handleBarMarkerClick={(long, lat) => handleMarkerClick(long, lat)}
+          />
         ))}
       </Map>
     </>
