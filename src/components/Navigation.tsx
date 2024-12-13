72aef565628445225d2bc66b78992e5f5bbec477
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-13 19:05:44 +0100

    Refactor Navigation component to use grid layout with popdown menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -13,28 +13,38 @@ export const Navigation = () => {
   }
 
   return (
-    <nav className="fixed top-0 z-[10] flex h-[70px] w-full justify-between bg-white">
-      <Link href={'/'} className="flex h-full items-center justify-center pl-4 text-4xl">
-        <Logo className="h-8" />
-      </Link>
-      <Hamburger isMenuOpen={isMenuOpen} handleClick={handleClick} />
-      {/* <ul className="flex h-full items-center justify-end gap-2">
-        <li>
-          <Link href={'/'} className="flex h-[70px] min-w-20 items-center px-4">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href={'/bars'} className="flex h-[70px] min-w-20 items-center px-4">
-            Bars
-          </Link>
-        </li>
-        <li>
-          <Link href={'/testing-ground'} className="flex h-[70px] min-w-20 items-center px-4">
-            Test
-          </Link>
-        </li>
-      </ul> */}
+    //GRID WITH POPDOWN MENU
+    <nav
+      className="fixed top-0 z-[10] grid w-full grid-cols-2 grid-rows-[70px,_0fr] justify-between bg-red-300 transition-all data-[open=true]:grid-rows-[70px,_1fr]"
+      data-open={isMenuOpen}
+    >
+      <div className="flex items-center justify-start pl-4">
+        <Link href={'/'} className="">
+          <Logo className="h-8 -translate-y-0.5" />
+        </Link>
+      </div>
+      <div className="flex items-center justify-end">
+        <Hamburger isMenuOpen={isMenuOpen} handleClick={handleClick} />
+      </div>
+      <div className="col-span-2 row-[2/3] overflow-hidden bg-slate-400">
+        <ul className="flex h-full items-center justify-end gap-2">
+          <li>
+            <Link href={'/'} className="flex h-[70px] min-w-20 items-center px-4">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href={'/bars'} className="flex h-[70px] min-w-20 items-center px-4">
+              Bars
+            </Link>
+          </li>
+          <li>
+            <Link href={'/testing-ground'} className="flex h-[70px] min-w-20 items-center px-4">
+              Test
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
