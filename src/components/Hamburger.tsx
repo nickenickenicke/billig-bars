981d1084ba64d2b1f59796ddf7de4fbb373633e9
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-13 17:42:34 +0100

    Refactored data attribute name

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -11,12 +11,12 @@ export const Hamburger = ({ isMenuOpen, handleClick }: HamburgerProps) => {
   return (
     <button
       className="group aspect-square border-none"
-      data-open={isMenuOpen}
+      data-is-menu-open={isMenuOpen}
       onClick={handleClick}
     >
-      <div className="block h-0.5 w-8 -translate-y-2 bg-black transition group-data-[open=true]:translate-y-0.5"></div>
+      <div className="block h-0.5 w-8 -translate-y-2 bg-black transition group-data-[is-menu-open=true]:translate-y-0.5"></div>
       <div className="block h-0.5 w-8 bg-black"></div>
-      <div className="block h-0.5 w-8 translate-y-2 bg-black transition group-data-[open=true]:-translate-y-0.5"></div>
+      <div className="block h-0.5 w-8 translate-y-2 bg-black transition group-data-[is-menu-open=true]:-translate-y-0.5"></div>
     </button>
   )
 }
