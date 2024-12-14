e045762f31a9724dbb0c31a102d9aa5052c5b9ce
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-14 14:37:07 +0100

    Layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { BarsList } from '@/components/BarsList'
 import { GeolocateButton } from '@/components/GeolocateButton'
+import { Mascot } from '@/components/svgs/Mascot'
 
 export default function Home() {
   return (
-    <article className="px-2">
-      <GeolocateButton />
-      <BarsList />
+    <article className="-mt-navbar flex min-h-[90svh] items-center justify-center px-2">
+      <Mascot className="w-2/3" />
     </article>
   )
 }
