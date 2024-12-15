e80d78bbea380742d2934b1da17d1f6a83e92001
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-15 13:59:08 +0100

    Minor styling edits

--- src/components/BarCardPill.tsx
+++ src/components/BarCardPill.tsx
@@ -4,7 +4,7 @@ interface BarCardPillProps {
 
 export const BarCardPill = ({ children }: BarCardPillProps) => {
   return (
-    <span className="block rounded-full border border-green-price p-1 px-2.5 text-xs uppercase tracking-wide text-green-price">
+    <span className="rounded-full border border-green-price p-1 px-2.5 text-xs uppercase tracking-wide text-green-price">
       {children}
     </span>
   )
