be3c39010e6f9e5cd92582d5923ea901ab3607d0
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-20 14:31:05 +0100

    Date range is now properly displayed

--- src/components/BarHappyHours.tsx
+++ src/components/BarHappyHours.tsx
@@ -1,5 +1,8 @@
 import { HappyHours } from '@/models/Bar'
 import { getWeekdayName, normalizeTimeFromDB } from '@/utils/timeTools'
+import { Fragment } from 'react'
+import { BeerPriceCircle } from './BeerPriceCircle'
+import { BeerStats } from './BeerStats'
 
 interface BarHappyHoursProps {
   happyHours: HappyHours[]
@@ -19,7 +22,7 @@ type GroupedHappyHours = {
 export const BarHappyHours = ({ happyHours }: BarHappyHoursProps) => {
   if (!happyHours.length) return null
 
-  const groupHappyHours = (happyHoursToGroup: HappyHours[]) => {
+  const groupHappyHours = (happyHoursToGroup: HappyHours[]): GroupedHappyHours[] => {
     const happyHoursGroups: Record<string, GroupedHappyHours> = {}
 
     happyHoursToGroup.forEach(happyHour => {
@@ -42,22 +45,43 @@ export const BarHappyHours = ({ happyHours }: BarHappyHoursProps) => {
     return Object.values(happyHoursGroups)
   }
 
+  const getDayRange = (days: number[]): string => {
+    const sortedDays = [...days].sort((a, b) => a - b)
+
+    const isConsecutive = sortedDays.every(
+      (day, index) => index === 0 || day === sortedDays[index - 1] + 1
+    )
+
+    if (isConsecutive) {
+      const startDay = getWeekdayName(sortedDays[0])
+      const endDay = getWeekdayName(sortedDays[sortedDays.length - 1])
+      return startDay === endDay ? startDay : `${startDay} - ${endDay}`
+    }
+
+    return sortedDays.map(day => getWeekdayName(day)).join(', ')
+  }
+
   const groupedHappyHours = groupHappyHours(happyHours)
 
   return (
     <>
-      <ul>
+      <ul className="grid grid-cols-[auto_1fr] gap-2">
+        <li className="col-span-2">
+          <h3 className="font-medium uppercase">Happy hours</h3>
+        </li>
         {groupedHappyHours.map((group, i) => {
           return (
-            <li key={i}>
-              <span className="block">{JSON.stringify(group.days)}</span>
-              <span className="block">
-                {group.starts_at}.{group.starts_at_min} - {group.ends_at}.{group.ends_at_min}
-              </span>
-              <span className="block">
-                {group.price}kr , {group.volume}cl, {group.ppv}kr/cl
-              </span>
-            </li>
+            <Fragment key={i}>
+              <li className="block">{getDayRange(group.days)}</li>
+              <li className="block">
+                {normalizeTimeFromDB(group.starts_at, group.starts_at_min)} -{' '}
+                {normalizeTimeFromDB(group.ends_at, group.ends_at_min)}
+              </li>
+              <li className="col-span-2 flex gap-2">
+                <BeerPriceCircle beer_ppv={group.ppv} beer_price={group.price} small />
+                <BeerStats beer_ppv={group.ppv} beer_volume={group.volume} />
+              </li>
+            </Fragment>
           )
         })}
       </ul>
