ff9403f045a8fbfd7375d4209588dadd8327a7c0
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-20 14:10:04 +0100

    Started refactoring work on BarHappyHours
    Should group days together and display them as one thing

--- src/components/BarHappyHours.tsx
+++ src/components/BarHappyHours.tsx
@@ -2,20 +2,61 @@ import { HappyHours } from '@/models/Bar'
 import { getWeekdayName, normalizeTimeFromDB } from '@/utils/timeTools'
 
 interface BarHappyHoursProps {
-  happyHours?: HappyHours[]
+  happyHours: HappyHours[]
 }
+
+type GroupedHappyHours = {
+  price: number
+  volume: number
+  ppv: number
+  starts_at: number
+  starts_at_min: number
+  ends_at: number
+  ends_at_min: number
+  days: number[]
+}
+
 export const BarHappyHours = ({ happyHours }: BarHappyHoursProps) => {
-  if (!happyHours || happyHours.length === 0) return null
+  if (!happyHours.length) return null
+
+  const groupHappyHours = (happyHoursToGroup: HappyHours[]) => {
+    const happyHoursGroups: Record<string, GroupedHappyHours> = {}
+
+    happyHoursToGroup.forEach(happyHour => {
+      const key = `${happyHour.price}-${happyHour.volume}-${happyHour.ppv}-${happyHour.starts_at}-${happyHour.starts_at_min}-${happyHour.ends_at}-${happyHour.ends_at_min}`
+      if (!happyHoursGroups[key]) {
+        happyHoursGroups[key] = {
+          price: happyHour.price,
+          volume: happyHour.volume,
+          ppv: happyHour.ppv,
+          starts_at: happyHour.starts_at,
+          starts_at_min: happyHour.starts_at_min,
+          ends_at: happyHour.ends_at,
+          ends_at_min: happyHour.ends_at_min,
+          days: []
+        }
+      }
+      happyHoursGroups[key].days.push(happyHour.day_of_week)
+    })
+
+    return Object.values(happyHoursGroups)
+  }
+
+  const groupedHappyHours = groupHappyHours(happyHours)
 
   return (
     <>
       <ul>
-        {happyHours.map((happyHour, i) => {
+        {groupedHappyHours.map((group, i) => {
           return (
-            <li key={happyHour.id!}>
-              {`${getWeekdayName(happyHour.day_of_week)}: ${normalizeTimeFromDB(happyHour.starts_at, happyHour.starts_at_min)} - ${normalizeTimeFromDB(happyHour.ends_at, happyHour.ends_at_min)}`}
-              <br />
-              {`Volym: ${happyHour.volume}cl, pris: ${happyHour.price}kr, pris per cl: ${happyHour.ppv}kr`}
+            <li key={i}>
+              <span className="block">{JSON.stringify(group.days)}</span>
+              <span className="block">
+                {group.starts_at}.{group.starts_at_min} - {group.ends_at}.{group.ends_at_min}
+              </span>
+              <span className="block">
+                {group.price}kr , {group.volume}cl, {group.ppv}kr/cl
+              </span>
             </li>
           )
         })}
