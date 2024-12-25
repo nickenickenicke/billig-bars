a018ff1600789b604a9410bdc8027475e92786d7
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-25 15:38:33 +0100

    Added white background to component

--- src/components/BarPill.tsx
+++ src/components/BarPill.tsx
@@ -6,7 +6,7 @@ interface BarPillProps {
 export const BarPill = ({ children, red }: BarPillProps) => {
   return (
     <span
-      className={`rounded-full border p-1 px-2.5 text-xs uppercase tracking-wide ${red ? 'border-red-price text-red-price' : 'border-green-price text-green-price'}`}
+      className={`rounded-full border bg-white p-1 px-2.5 text-xs uppercase tracking-wide ${red ? 'border-red-price text-red-price' : 'border-green-price text-green-price'}`}
     >
       {children}
     </span>
