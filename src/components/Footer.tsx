933ab8345eb1ff29282534d781115fa279f3169e
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-25 15:31:04 +0100

    Adjusted grid

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
     <footer className="mt-8 flex w-full flex-col items-center">
       <SvgFooter className="-mb-1 max-h-24 max-w-[100vw]" />
       <div className="grid min-h-40 w-full grid-cols-[auto] grid-rows-[auto_auto_auto_1fr] gap-10 bg-black px-4 pb-20 pt-6 text-white md:grid-cols-[15rem_auto_1fr] md:grid-rows-[auto_1fr] md:gap-6">
-        <figure className="row-span-1 md:col-[1/3] md:row-[1/2]">
+        <figure className="row-span-1 md:col-[1/2] md:row-[1/2]">
           <SvgLogo className="h-6 fill-white" />
         </figure>
         <nav className="row-span-1 flex flex-col gap-3 text-xl tracking-wide md:col-[1/2] md:row-[2/3]">
@@ -67,7 +67,7 @@ export const Footer = () => {
             .
           </p>
         </section>
-        <ScrollToTop className="row-span-1 cursor-pointer justify-self-end underline underline-offset-4 hover:text-green-price md:col-[3/4] md:row-[1/2]">
+        <ScrollToTop className="row-span-1 cursor-pointer justify-self-end underline underline-offset-4 hover:text-green-price md:col-[2/4] md:row-[1/2]">
           Tillbaka till starten av sidan
         </ScrollToTop>
       </div>
