88396f45c164a8dddcc1f82d3b0e16837a694349
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-25 15:22:46 +0100

    Started work on desktop layout

--- src/app/bars/[id]/page.tsx
+++ src/app/bars/[id]/page.tsx
@@ -55,47 +55,51 @@ export default async function BarPage({ params, searchParams }: BarPageProps) {
 
     return (
       <>
-        <MapCanvas singleBar={bar} />
-        <article className="mt-4 px-2">
-          <span className="mb-4 block text-right text-sm">
-            <Link href={'/bars'} className="underline underline-offset-2">
-              {'< '}Tillbaka till alla barer
-            </Link>
-          </span>
-
-          <div className="grid grid-cols-[1fr_1fr_auto_100px] grid-rows-[1fr_auto_50px] justify-between rounded-none bg-white">
-            <div className="col-[1/3] row-[1/3]">
-              <h1 className="text-4xl font-medium">{bar.name}</h1>
-              {bar.dist_meters && bar.dist_meters > 0 ? (
-                <span>{normalizeMeters(bar.dist_meters)}</span>
-              ) : null}
-              <address>
-                {bar.address}
-                <br />
-                {normalizePostalCode(bar.postal_code)} {bar.city}
-              </address>
-              <span className="block">
-                {bar.is_open
-                  ? 'Öppet till ' + getClosingHour(bar.opening_hours)
-                  : getOpeningHour(bar.opens_at)}
-              </span>
-            </div>
-
-            <div className="col-[1/4] row-[3/4] flex flex-row-reverse gap-2 pr-2">
-              <BeerStats beer_ppv={bar.current_ppv} beer_volume={bar.current_ppv} />
-            </div>
-
-            <div className="col-[3/5] row-[1/2] flex flex-col flex-wrap items-end justify-start gap-2 pt-1.5">
-              <BarPills is_open={bar.is_open} is_happy_hour={bar.is_happy_hour} />
-            </div>
-
-            <div className={`col-[4/5] row-[2/4] mt-4 flex flex-col items-end justify-end gap-2`}>
-              <BeerPriceCircle beer_ppv={bar.current_ppv} beer_price={bar.current_price} />
-            </div>
+        <section className="grid grid-cols-1 grid-rows-[auto_auto] md:grid-cols-[1fr_1fr] md:grid-rows-[auto] md:items-start">
+          <div className="aspect-[18/15] md:sticky md:left-0 md:top-navbar md:col-[2/3] md:row-[1/2] md:aspect-[unset] md:h-[calc(100vh-var(--navbar-height))] md:w-[50vw]">
+            <MapCanvas singleBar={bar} />
           </div>
-          {bar.happy_hours && bar.happy_hours.length > 0 && <BarPriceInformation bar={bar} />}
-          <BarOpeningHours openingHours={bar.opening_hours} />
-        </article>
+          <article className="mt-4 px-2 md:col-[1/2] md:row-[1/2]">
+            <span className="mb-4 block text-right text-sm">
+              <Link href={'/bars'} className="underline underline-offset-2">
+                {'< '}Tillbaka till alla barer
+              </Link>
+            </span>
+
+            <div className="grid grid-cols-[1fr_1fr_auto_100px] grid-rows-[1fr_auto_50px] justify-between rounded-none bg-white">
+              <div className="col-[1/3] row-[1/3]">
+                <h1 className="text-4xl font-medium">{bar.name}</h1>
+                {bar.dist_meters && bar.dist_meters > 0 ? (
+                  <span>{normalizeMeters(bar.dist_meters)}</span>
+                ) : null}
+                <address>
+                  {bar.address}
+                  <br />
+                  {normalizePostalCode(bar.postal_code)} {bar.city}
+                </address>
+                <span className="block">
+                  {bar.is_open
+                    ? 'Öppet till ' + getClosingHour(bar.opening_hours)
+                    : getOpeningHour(bar.opens_at)}
+                </span>
+              </div>
+
+              <div className="col-[1/4] row-[3/4] flex flex-row-reverse gap-2 pr-2">
+                <BeerStats beer_ppv={bar.current_ppv} beer_volume={bar.current_ppv} />
+              </div>
+
+              <div className="col-[3/5] row-[1/2] flex flex-col flex-wrap items-end justify-start gap-2 pt-1.5">
+                <BarPills is_open={bar.is_open} is_happy_hour={bar.is_happy_hour} />
+              </div>
+
+              <div className={`col-[4/5] row-[2/4] mt-4 flex flex-col items-end justify-end gap-2`}>
+                <BeerPriceCircle beer_ppv={bar.current_ppv} beer_price={bar.current_price} />
+              </div>
+            </div>
+            {bar.happy_hours && bar.happy_hours.length > 0 && <BarPriceInformation bar={bar} />}
+            <BarOpeningHours openingHours={bar.opening_hours} />
+          </article>
+        </section>
       </>
     )
   }
--- src/components/BarsView.tsx
+++ src/components/BarsView.tsx
@@ -10,13 +10,17 @@ import { MapCanvas } from './MapCanvas'
 export const BarsView = () => {
   return (
     <MapProvider>
-      <MapCanvas />
-      <article className="px-2">
-        <FilterBar />
-        <GeolocateButton />
-        <FilterStatus />
-        <BarsList />
-      </article>
+      <section className="grid grid-cols-1 grid-rows-[auto_auto] md:grid-cols-[1fr_1fr] md:grid-rows-[auto] md:items-start">
+        <div className="aspect-[18/15] md:sticky md:left-0 md:top-navbar md:col-[2/3] md:row-[1/2] md:aspect-[unset] md:h-[calc(100vh-var(--navbar-height))] md:w-[50vw]">
+          <MapCanvas />
+        </div>
+        <article className="px-2 md:col-[1/2] md:row-[1/2]">
+          <FilterBar />
+          <GeolocateButton />
+          <FilterStatus />
+          <BarsList />
+        </article>
+      </section>
     </MapProvider>
   )
 }
