0543dc3de71ac0503c78090edea32a5dcf5957a0
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-26 22:09:06 +0100

    Desktop sizing

--- src/components/MapPopup.tsx
+++ src/components/MapPopup.tsx
@@ -25,14 +25,15 @@ export const MapPopup = ({ bar, currentLocation, handleClosePopup, singleBar }: 
       offset={30}
       className="overflow-x-hidden font-sans"
       closeButton={singleBar ? false : true}
+      maxWidth="none"
     >
       <Link
-        className="col-[1/3] row-[1/2] pt-1 text-lg font-medium focus-within:outline-none"
+        className="col-[1/3] row-[1/2] pt-1 text-lg font-medium focus-within:outline-none md:text-2xl"
         href={`/bars/${bar.id}${currentLocation.currentlat !== 0 ? `?currentlat=${currentLocation.currentlat}&currentlong=${currentLocation.currentlong}` : ''}`}
       >
         {bar.name}
       </Link>
-      <div className="col-[1/2] row-[2/3] pr-4 text-sm font-normal leading-5">
+      <div className="col-[1/2] row-[2/3] pr-4 text-sm font-normal leading-5 md:mt-1 md:text-base">
         <span className="block">{bar.address}</span>
         {bar.dist_meters && bar.dist_meters != -1 ? (
           <span className="block">{normalizeMeters(bar.dist_meters)}</span>
@@ -45,16 +46,16 @@ export const MapPopup = ({ bar, currentLocation, handleClosePopup, singleBar }: 
 
         {!singleBar && (
           <Link
-            className="mt-1 block underline"
+            className="mt-1 block underline underline-offset-2"
             href={`/bars/${bar.id}${currentLocation.currentlat !== 0 ? `?currentlat=${currentLocation.currentlat}&currentlong=${currentLocation.currentlong}` : ''}`}
           >
             Mer information
           </Link>
         )}
       </div>
-      <div className="col-[2/4] row-[2/3] place-self-end pr-1">
+      <div className="col-[2/4] row-[2/3] place-self-end pr-1 md:pl-4">
         <BeerPriceCircle beer_ppv={bar.current_ppv} beer_price={bar.current_price} small />
-        <span className="block text-center">{bar.beer_volume} cl</span>
+        <span className="block text-center md:text-base">{bar.beer_volume} cl</span>
       </div>
     </Popup>
   )
