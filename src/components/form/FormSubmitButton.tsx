d14b0df12ed6013312332afee13c64a47a394a4e
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-27 17:07:00 +0100

    Add optional className prop to FormSubmitButton

--- src/components/form/FormSubmitButton.tsx
+++ src/components/form/FormSubmitButton.tsx
@@ -4,13 +4,14 @@ import { Button } from '../Button'
 type FormSubmitButtonProps = {
   label: string
   loadingMessage: React.ReactNode
+  className?: string
 }
 
-export const FormSubmitButton = ({ label, loadingMessage }: FormSubmitButtonProps) => {
+export const FormSubmitButton = ({ label, loadingMessage, className }: FormSubmitButtonProps) => {
   const { pending } = useFormStatus()
 
   return (
-    <Button type="submit" disabled={pending} className="">
+    <Button type="submit" disabled={pending} className={`${className ? className : ''}`}>
       {pending ? loadingMessage : label}
     </Button>
   )
