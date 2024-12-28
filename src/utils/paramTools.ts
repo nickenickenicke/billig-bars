8cefb263b472f586bc9055e656b2fa3557b6807a
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-28 01:48:17 +0100

    Refactor checkParams to simplify validation logic for min, hour, and day parameters

--- src/utils/paramTools.ts
+++ src/utils/paramTools.ts
@@ -15,26 +15,26 @@ export const checkParams = (params: URLSearchParams): CurrentQuery => {
   let min: number | null = null
   if (params.get('min')) {
     min = parseInt(params.get('min') as string) || min
-    if (min !== null && (min < 0 || min > 59)) {
-      min = getCurrentMinute()
-    }
+  }
+  if (min === null || min < 0 || min > 59) {
+    min = getCurrentMinute()
   }
   min = adjustMinutesToHalfHour(min || 0)
 
   let hour: number | null = null
   if (params.get('hour')) {
     hour = parseInt(params.get('hour') as string) || hour
-    if (hour !== null && (hour < 0 || hour > 23)) {
-      hour = getCurrentHour()
-    }
+  }
+  if (hour === null || hour < 0 || hour > 23) {
+    hour = getCurrentHour()
   }
 
   let day: number | null = null
   if (params.get('day')) {
     day = parseInt(params.get('day') as string) || day
-    if (day !== null && (day < 0 || day > 7)) {
-      day = getTodaysWeekday()
-    }
+  }
+  if (day === null || day < 0 || day > 7) {
+    day = getTodaysWeekday()
   }
 
   return {
