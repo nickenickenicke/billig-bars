70c7280a0fd17520d74072d834bc7e0ed52514d2
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-29 14:14:38 +0100

    Add unknown error message for geolocation errors

--- src/lib/errorMessages.ts
+++ src/lib/errorMessages.ts
@@ -1,4 +1,5 @@
 export const GeolocationPositionErrorMessage: string[] = [
+  'Okänt fel',
   'Du nekade åtkomst till din position',
   'Kunde inte hitta din position',
   'Det tog för lång tid att hitta din position'
