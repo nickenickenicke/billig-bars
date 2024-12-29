8e1684b9d91136eca95705aca98cf29624544b4a
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-29 14:11:46 +0100

    Added error messages for geolocation

---
+++ src/lib/errorMessages.ts
@@ -0,0 +1,5 @@
+export const GeolocationPositionErrorMessage: string[] = [
+  'Du nekade åtkomst till din position',
+  'Kunde inte hitta din position',
+  'Det tog för lång tid att hitta din position'
+]
