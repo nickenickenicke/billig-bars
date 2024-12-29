c7c5c84d7cb4e0e64bd6b8466e83d6aa81008c7e
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-29 14:07:04 +0100

    Remove unused price calculation functions from priceTools

--- src/utils/priceTools.ts
+++ src/utils/priceTools.ts
@@ -1,20 +1,3 @@
-import { HappyHourPriceDetails, HappyHours } from '@/models/Bar'
-import { getTodaysWeekday } from './timeTools'
-
-export const getPricePerCl = (price: number, volume: number) => {
-  return Math.round((price / volume) * 1000) / 1000
-}
-
-export const getHappyHourPrice = (happyHours: HappyHours[]): HappyHourPriceDetails => {
-  const currentDay = getTodaysWeekday()
-
-  const happyHour = happyHours.find(happyHour => happyHour.day_of_week === currentDay)
-  //TODO: handle this potential error better
-  if (!happyHour) return { price: 999, volume: 999, ppv: 999 }
-
-  return { price: happyHour.price, volume: happyHour.volume, ppv: happyHour.ppv }
-}
-
 export const calculatePPV = (price: string, volume: string): number => {
   const priceInt = parseInt(price)
   const volumeInt = parseInt(volume)
