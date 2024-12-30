31a919dcbf1a56df0f88542db57e5315dab9f9a6
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-30 16:43:06 +0100

    Refactor FilterBar and FilterByHour components to handle hour selection and routing

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -4,8 +4,8 @@ import { FilterByHour } from '@/components/FilterByHour'
 import { FilterButton } from './FilterButton'
 import { useState } from 'react'
 import { FilterStatus } from './FilterStatus'
-import { useSearchParams } from 'next/navigation'
-import { checkFilterSearchParams } from '@/utils/filterTools'
+import { useSearchParams, useRouter } from 'next/navigation'
+import { checkFilterSearchParams, createParamString } from '@/utils/filterTools'
 
 export interface FilterSearchParams {
   day: string
@@ -17,8 +17,24 @@ export interface FilterSearchParams {
 export const FilterBar = () => {
   const [isOpen, setIsOpen] = useState(false)
   const searchParams = useSearchParams()
+  const router = useRouter()
 
   const filterSearchParams = checkFilterSearchParams(searchParams)
+
+  const handleFilterChange = (param: string, e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (param !== 'hour') return
+    switch (e.target.value) {
+      case '-1': {
+        break
+      }
+      default:
+        if (filterSearchParams[param] === e.target.value) break
+        const newParamString = createParamString(filterSearchParams, param, e.target.value)
+        router.push('/bars' + newParamString, { scroll: false })
+        break
+    }
+    console.log(param, e.target.value)
+  }
 
   const toggleFilterBar = () => {
     setIsOpen(!isOpen)
@@ -34,7 +50,7 @@ export const FilterBar = () => {
         </div>
         <div className="group-data-[filter-open=false]:border- overflow-hidden border-b border-black">
           <span className="block">Veckodag</span>
-          <FilterByHour />
+          <FilterByHour hour={filterSearchParams.hour} handleChange={handleFilterChange} />
           <span className="block">Visa bara barer som är öppna först</span>
           <span className="block">Sortera efter närmast, billigast</span>
           <FilterStatus />
--- src/components/FilterByHour.tsx
+++ src/components/FilterByHour.tsx
@@ -1,48 +1,29 @@
 'use client'
 
-import { GlobalStateContext } from '@/contexts/GlobalStateContext'
-import { useRouter } from 'next/navigation'
-import { useContext, useEffect, useState } from 'react'
+import { useState } from 'react'
 
-export const FilterByHour = () => {
-  const {
-    globalState: { currentQuery }
-  } = useContext(GlobalStateContext)
-  const [currentlySelectedHour, setCurrentlySelectedHour] = useState<string>(
-    currentQuery.hour === null ? 'now' : currentQuery.hour.toString()
-  )
-  const router = useRouter()
-  useEffect(() => {
-    if (currentQuery.hour === null && currentlySelectedHour === 'now') return
-    if (currentQuery.hour?.toString() === currentlySelectedHour) return
+interface FilterByHourProps {
+  hour: string
+  handleChange: (param: string, e: React.ChangeEvent<HTMLSelectElement>) => void
+}
 
-    setCurrentlySelectedHour(currentQuery.hour === null ? 'now' : currentQuery.hour.toString())
-  }, [currentQuery.hour, currentlySelectedHour])
-
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    switch (e.target.value) {
-      case 'now': {
-        if (currentQuery.hour === null) break
-        router.push('/bars?hour=', { scroll: false })
-        break
-      }
-      case '-1': {
-        break
-      }
-      default:
-        if (currentQuery.hour?.toString() === e.target.value) break
-        router.push('/bars?hour=' + e.target.value, { scroll: false })
-        break
-    }
-    setCurrentlySelectedHour(e.target.value)
-  }
+export const FilterByHour = ({ hour, handleChange }: FilterByHourProps) => {
+  const [currentlySelectedHour, setCurrentlySelectedHour] = useState<string>(hour)
 
   return (
     <>
       <label htmlFor="time-select">Klockan</label>
-      <select id="time-select" name="time" onChange={handleChange} value={currentlySelectedHour}>
+      <select
+        id="time-select"
+        name="time"
+        onChange={e => {
+          handleChange('hour', e)
+          setCurrentlySelectedHour(e.target.value)
+        }}
+        value={currentlySelectedHour}
+      >
         <option value={'-1'}>Välj</option>
-        <option value={'now'}>Nu</option>
+        <option value={''}>Nu</option>
         <option value={'11'}>11</option>
         <option value={'12'}>12</option>
         <option value={'13'}>13</option>
