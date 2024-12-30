89e3d44c99632892b4e114fecaec347a03e230b3
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-30 16:51:44 +0100

    Integrate FilterByWeekday component into FilterBar

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react'
 import { FilterStatus } from './FilterStatus'
 import { useSearchParams, useRouter } from 'next/navigation'
 import { checkFilterSearchParams, createParamString } from '@/utils/filterTools'
+import { FilterByWeekday } from './FilterByWeekday'
 
 export interface FilterSearchParams {
   day: string
@@ -22,18 +23,20 @@ export const FilterBar = () => {
   const filterSearchParams = checkFilterSearchParams(searchParams)
 
   const handleFilterChange = (param: string, e: React.ChangeEvent<HTMLSelectElement>) => {
-    if (param !== 'hour') return
-    switch (e.target.value) {
-      case '-1': {
-        break
+    if (param === 'hour' || param === 'day') {
+      console.log(param, e.target.value)
+
+      switch (e.target.value) {
+        case '-1': {
+          break
+        }
+        default:
+          if (filterSearchParams[param] === e.target.value) break
+          const newParamString = createParamString(filterSearchParams, param, e.target.value)
+          router.push('/bars' + newParamString, { scroll: false })
+          break
       }
-      default:
-        if (filterSearchParams[param] === e.target.value) break
-        const newParamString = createParamString(filterSearchParams, param, e.target.value)
-        router.push('/bars' + newParamString, { scroll: false })
-        break
     }
-    console.log(param, e.target.value)
   }
 
   const toggleFilterBar = () => {
@@ -50,6 +53,7 @@ export const FilterBar = () => {
         </div>
         <div className="group-data-[filter-open=false]:border- overflow-hidden border-b border-black">
           <span className="block">Veckodag</span>
+          <FilterByWeekday weekday={filterSearchParams.day} handleChange={handleFilterChange} />
           <FilterByHour hour={filterSearchParams.hour} handleChange={handleFilterChange} />
           <span className="block">Visa bara barer som är öppna först</span>
           <span className="block">Sortera efter närmast, billigast</span>
