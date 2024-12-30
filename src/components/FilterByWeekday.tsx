d8eecd671816b88da0e42dd79ee2532a15b0c375
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-30 16:51:28 +0100

    Add FilterByWeekday component for selecting weekdays

---
+++ src/components/FilterByWeekday.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { weekdayNames } from '@/lib/weekdays'
+import { useState } from 'react'
+
+interface FilterByWeekdayProps {
+  weekday: string
+  handleChange: (param: string, e: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+export const FilterByWeekday = ({ weekday, handleChange }: FilterByWeekdayProps) => {
+  const [currentlySelectedWeekday, setCurrentlySelectedWeekday] = useState<string>(weekday)
+
+  return (
+    <>
+      <label htmlFor="weekday-select">Veckodag</label>
+      <select
+        id="weekday-select"
+        name="weekday"
+        onChange={e => {
+          console.log('changing')
+
+          handleChange('day', e)
+          setCurrentlySelectedWeekday(e.target.value)
+        }}
+        value={currentlySelectedWeekday}
+      >
+        <option value={'-1'}>Välj</option>
+        <option value={''}>Idag</option>
+        {weekdayNames.map((weekdayName, index) => {
+          return (
+            <option key={weekdayName} value={index + 1}>
+              {weekdayName}
+            </option>
+          )
+        })}
+      </select>
+    </>
+  )
+}
