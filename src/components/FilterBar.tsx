a533effed61e736d9a425336ace72f22e0667899
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-30 13:58:54 +0100

    Creaeted FilterIcon component

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,9 +1,11 @@
 import { FilterByHour } from '@/components/FilterByHour'
+import { FilterIcon } from './FilterIcon'
 
 export const FilterBar = () => {
   return (
     <>
       <h3>FILTERING</h3>
+      <FilterIcon />
       <FilterByHour />
     </>
   )
