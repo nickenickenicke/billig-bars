bcdbd7923e6dd84eeb8e3bbe9e70fbc2e3bd6e3d
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-12-30 15:00:02 +0100

    Adjusted padding and desktop styling

--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -6,7 +6,10 @@ interface FilterButtonProps {
 
 export const FilterButton = ({ onClick }: FilterButtonProps) => {
   return (
-    <button className="flex items-center gap-2 px-7 py-2">
+    <button
+      className="flex items-center gap-2 border-none py-2 pl-7 pr-2 uppercase underline underline-offset-2 md:text-lg"
+      onClick={onClick}
+    >
       <span>Filtrering</span>
       <FilterIcon />
     </button>
