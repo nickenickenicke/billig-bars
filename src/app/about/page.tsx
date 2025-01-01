1d33ec116c0eb96ab33d1231579f411d49076b7a
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2025-01-01 17:36:24 +0100

    Copy edits

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,23 +24,32 @@ export default async function About() {
           </p>
         </section>
         <SvgVortex className="mx-auto my-8 block w-[min(100%,_300px)] md:my-14" />
-        <section className="mx-auto grid w-fit grid-cols-[auto,_100px] items-start gap-3 self-start rounded-lg bg-bg-pink px-3 py-4 md:grid-cols-[minmax(auto,_55ch),_auto] md:gap-6 md:px-6 md:py-8 md:text-xl">
-          <h2 className="col-span-2 text-2xl font-medium md:text-4xl">Prisjämförelse</h2>
-          <p className="md:max-w-[55ch]">
+        <section className="mx-auto grid w-fit grid-cols-[auto,_100px] items-start gap-x-3 gap-y-1 self-start rounded-lg bg-bg-pink px-3 py-4 md:grid-cols-[minmax(auto,_55ch),_auto] md:gap-x-6 md:px-6 md:py-8 md:text-xl">
+          <h2 className="col-span-2 mb-2 text-2xl font-medium md:mb-5 md:text-4xl">
+            Prisjämförelse
+          </h2>
+          <h3 className="col-span-2 text-xl font-medium md:text-2xl">Billig</h3>
+          <p className="max-w-[55ch] md:mb-5">
             Ett grönt pris betyder att ölen kostar 1.125 kr eller midre per centiliter. Det
-            mostvarar en vanlig 40 centiliter stor stark för 45 kr eller mindre. Tyvärr är det var
-            en billig bärs kostar nuförtiden.
+            mostvarar en vanlig 40 centiliter stor stark för 45 kr eller mindre.
+            <br />
+            Tyvärr är det var en billig bärs kostar nuförtiden.
           </p>
           <BeerPriceCircle beer_ppv={0.875} beer_price={35} example />
-          <p className="md:max-w-[55ch]">
+          <h3 className="col-span-2 text-xl font-medium md:text-2xl">Mellan</h3>
+          <p className="max-w-[55ch] md:mb-5">
             Ett gult pris betyder att ölen kostar mellan 1.125 kr och 1.5 kr per centiliter. Det
-            mostvarar en vanlig 40 centiliter stor stark för mellan 45 kr och 60 kr. Ett pris som
-            tar emot men ändå är acceptabelt.
+            mostvarar en vanlig 40 centiliter stor stark för mellan 45 kr och 60 kr.
+            <br />
+            Ett pris som tar emot men ändå är acceptabelt.
           </p>
           <BeerPriceCircle beer_ppv={1.375} beer_price={55} example />
-          <p className="md:max-w-[55ch]">
+          <h3 className="col-span-2 text-xl font-medium md:text-2xl">Dyr</h3>
+          <p className="max-w-[55ch]">
             Ett rött pris betyder att ölen kostar över 1.50 kr per centiliter. Det mostvarar en
-            vanlig 40 centiliter stor stark för över 60 kr. Alldeles för dyrt!
+            vanlig 40 centiliter stor stark för över 60 kr.
+            <br />
+            Alldeles för dyrt!
           </p>
           <BeerPriceCircle beer_ppv={2.225} beer_price={89} example />
         </section>
