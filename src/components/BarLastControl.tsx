e435efb41272ed4bd05ba79a300c5984b65b82fe
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2025-01-06 13:28:04 +0100

    Update BarLastControl component to use controlled_at prop and format date

--- src/components/BarLastControl.tsx
+++ src/components/BarLastControl.tsx
@@ -1,13 +1,16 @@
 interface BarLastUpdatedProps {
   barName: string
-  last_controlled: string
+  controlled_at: string
 }
 
-export const BarLastControl = ({ barName, last_controlled }: BarLastUpdatedProps) => {
+export const BarLastControl = ({ barName, controlled_at }: BarLastUpdatedProps) => {
+  const lastControl = new Date(controlled_at).toLocaleDateString('sv-SE', {
+    timeZone: 'Europe/Stockholm'
+  })
   return (
     <div className="flex flex-col items-end justify-end text-sm italic">
       <span>{barName} kontrollerades senast</span>
-      <span>{last_controlled}</span>
+      <span>{lastControl}</span>
     </div>
   )
 }
