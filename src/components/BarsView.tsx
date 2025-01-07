8623dde0eae65478b5801b6261815433d28e27b4
nickenickenicke <nickenickebergman@gmail.com>
2025-01-07 11:45:23 +0100

    Made map slightly larger on mobile to offset new buttons

--- src/components/BarsView.tsx
+++ src/components/BarsView.tsx
@@ -9,7 +9,7 @@ export const BarsView = () => {
   return (
     <MapProvider>
       <section className="grid grid-cols-1 grid-rows-[auto_auto] md:grid-cols-[minmax(40vw,_400px)_1fr] md:grid-rows-[auto] md:items-start">
-        <div className="aspect-[18/15] md:sticky md:left-0 md:top-navbar md:col-[2/3] md:row-[1/2] md:aspect-[unset] md:h-[calc(100vh-var(--navbar-height))] md:w-full">
+        <div className="aspect-[17/15] md:sticky md:left-0 md:top-navbar md:col-[2/3] md:row-[1/2] md:aspect-[unset] md:h-[calc(100vh-var(--navbar-height))] md:w-full">
           <MapCanvas />
         </div>
         <article className="px-2 md:col-[1/2] md:row-[1/2] md:px-4 lg:px-8">
