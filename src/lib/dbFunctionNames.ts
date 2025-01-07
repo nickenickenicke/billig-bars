20710f60e920b5f2974fbaaed3e267c58c97991c
nickenickenicke <nickenickebergman@gmail.com>
2025-01-07 11:00:09 +0100

    New database functions

--- src/lib/dbFunctionNames.ts
+++ src/lib/dbFunctionNames.ts
@@ -1,4 +1,4 @@
-export const DB_SINGLE_BAR_FUNCTION = 'barlastcontrol'
-export const DB_ALL_BARS_FUNCTION = 'barslastcontrol'
+export const DB_SINGLE_BAR_FUNCTION = 'barcontrolled'
+export const DB_ALL_BARS_FUNCTION = 'barscontrolled'
 export const DB_UPSERT_BAR_FUNCTION = 'upsert_bar_details'
 export const DB_AUTH_CHECK_ROLE_FUNCTION = 'check_user_role'
