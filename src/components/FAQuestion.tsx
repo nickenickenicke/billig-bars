30027327632cee8f96f0d8e99d994daa965fcd1a
nickenickenicke <nickenickebergman@gmail.com>
2025-01-13 15:26:57 +0100

    Added chevron

--- src/components/FAQuestion.tsx
+++ src/components/FAQuestion.tsx
@@ -5,9 +5,19 @@ interface FAQuestionProps {
 
 export const FAQuestion = ({ summary, children }: FAQuestionProps) => {
   return (
-    <details className="group w-full border-b border-black">
-      <summary className="cursor-pointer list-none items-end pb-[0.5lh] pt-[1lh] group-open:text-red-400">
+    <details className="group w-full border-b border-black pt-[1lh] first-of-type:pt-0">
+      <summary className="flex cursor-pointer list-none items-center justify-between pb-[0.5lh] font-medium">
         {summary}
+        <span className="block h-full pl-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            className="rotate-90 transition-transform group-open:rotate-[270deg]"
+          >
+            <path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" />
+          </svg>
+        </span>
       </summary>
       {children}
     </details>
